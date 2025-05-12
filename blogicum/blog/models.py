8e2b249b28e43e5042236941f7933426a96303c0
SofiaPurtova <sofiapurtova@yandex.ru>
2025-05-12 20:41:23 +0300

    исправлены ошибки

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -24,7 +24,8 @@ class Category(models.Model):
     )
     is_published = models.BooleanField(
         default=True,
-        verbose_name='Опубликовано'
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
     )
     created_at = models.DateTimeField(
         auto_now_add=True,
